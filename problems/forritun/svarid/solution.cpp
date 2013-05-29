2ed2bbad9844449d9cb3b64ea78b2965e1c498c5
SuprDewd <suprdewd@gmail.com>
2013-05-29 12:04:32 +0000

    Added problems: hallo_heimur, svarid, svarid_ekki

---
+++ problems/forritun/svarid/solution.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "Svarið við spurningunni um tilgang lífsins, alheimsins, og alls er " << n << "." << endl;
+    return 0;
+}
---
+++ problems/forritun/svarid_ekki/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n == 42)
+    {
+        cout << "Svarið við spurningunni um tilgang lífsins, alheimsins, og alls er 42." << endl;
+    }
+    else
+    {
+        cout << "Svarið við spurningunni um tilgang lífsins, alheimsins, og alls er ekki " << n << "." << endl;
+    }
+
+    return 0;
+}
