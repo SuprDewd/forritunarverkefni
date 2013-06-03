e5f90d05b479439d31d102a8df423cbfbeab56e9
SuprDewd <suprdewd@gmail.com>
2013-06-03 15:50:04 +0000

    Fixed test_with_checker.sh

--- test/test_with_checker.sh
+++ test/test_with_checker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CUR=$(dirname $(readlink -f $0))
 FILE=$(readlink -f $1)
 DIR=$(dirname $FILE)
 EXE=/tmp/exe$RANDOM
@@ -8,7 +9,7 @@ g++ $FILE -o $EXE && for t in $DIR/tests/*.in; do
     echo "input: $(basename $t)"
     OBT=/tmp/obt$RANDOM
     $EXE < $t > $OBT
-    python2 run_checker.py "$DIR/checker.py" "$OBT" "${t%*.in}.out"
+    python2 "$CUR/run_checker.py" "$DIR/checker.py" "$OBT" "${t%*.in}.out"
     rm -f $OBT
 done
 
